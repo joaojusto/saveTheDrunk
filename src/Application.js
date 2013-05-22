e0045b215f8d74696bab118b3a62da0f73264db6
Jvxtv <jpjustonunes@hotmail.com>
2013-05-22 16:49:05 +0100

    - Added coments to the code;

--- src/Application.js
+++ src/Application.js
@@ -8,17 +8,23 @@ exports = Class(GC.Application, function () {
 		
 		this.style.backgroundColor = "#FFFFFF";
 		
+		//Drunk array;
 		this.drunks = [new Drunk({superview: GC.app.view, x: 20, y: 20}), new Drunk({superview: GC.app.view, x: 100, y: 100}),];
 		
+		//Controls the touch target
 		this.target = -1;
+		
+		//Controls if recording trails or not
 		this.recording = false;
 		
 		this.view.on("InputStart", function (evt, pt) {
 		
+			//Checks if the touch was over any element in the drunk array;
 			for (r = 0; r < GC.app.drunks.length; r++) {
 				
 				var drunk = GC.app.drunks[r];
 				
+				//If the touch was over any drunk, cleans the old trail and sets recording and target to start recording
 				if (drunk.containsLocalPoint (pt)) {
 					
 					GC.app.target = r;
@@ -33,6 +39,7 @@ exports = Class(GC.Application, function () {
 		
 		this.view.on("InputMove", function (evt, pt) {
 			
+			//adds the new trails to the drunk target
 			if (GC.app.recording == true) {
 
 				var opts = {superview: GC.app.view, x: pt.x - 3, y: pt.y - 3};
@@ -41,6 +48,7 @@ exports = Class(GC.Application, function () {
 			} 
 		});
 		
+		//touch ends, disable record and target
 		this.view.on("InputOut", function (over, overCount, atTarget) {
 			
 			GC.app.target = -1;
