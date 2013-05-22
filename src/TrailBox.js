70e436ad2d4342e81814de5070979c64caee4b86
Jvxtv <jpjustonunes@hotmail.com>
2013-05-22 13:28:32 +0100

    - Adicionada a classe TrailBox

---
+++ src/TrailBox.js
@@ -0,0 +1,24 @@
+import ui.View as View;
+
+exports = new Class (View, function (supr) {
+	
+	this.init = function (opts) {
+		
+		supr(this, "init", [merge(opts, {width: 6, height: 6, backgroundColor: "#008800"})]);
+
+		this._dt = 0;
+	};
+
+	this.reset = function (opts) {
+
+		this._dt = 0;
+
+		this.updateOpts(opts);
+	};
+	
+	this.tick = function (dt) {
+		
+		this._dt += dt;
+		
+	};
+});
